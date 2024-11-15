5cdc6910b043fb2bae3b992be1408471ca2853fc
Pheletso Mashalan <mashalaneevanspheletso@gmail.com>
2024-11-16 00:15:15 +0200

    constants

--- app/src/main/java/com/loc/newsapp/util/Constants.kt
+++ app/src/main/java/com/loc/newsapp/util/Constants.kt
@@ -4,4 +4,7 @@ object Constants {
 
     const val USER_SETTINGS = "userSettings"
     const val APP_ENTRY = "appEntry"
+    const val  API_KEY = "bc9b8c26691540bebe6ccfa809525b3d"
+    const val  STARTING_PAGE_INDEX = 1
+    const val BASE_URL = "https://newsapi.org/v2/"
 }
