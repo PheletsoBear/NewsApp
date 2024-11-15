b5dab3f40e7dd141f84f247c43c7f7373fa12510
Pheletso Mashalan <mashalaneevanspheletso@gmail.com>
2024-11-16 00:59:04 +0200

    added the pagingSource

--- app/src/main/java/com/loc/newsapp/data/remote/SearchNewsPagingSource.kt
+++ app/src/main/java/com/loc/newsapp/data/remote/SearchNewsPagingSource.kt
@@ -1,4 +1,43 @@
 package com.loc.newsapp.data.remote
 
-class SearchNewsPagingSource {
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import androidx.room.Query
+import com.loc.newsapp.domain.model.Article
+import com.loc.newsapp.util.Constants.STARTING_PAGE_INDEX
+
+class SearchNewsPagingSource(
+    private val newsApi: NewsApi,
+    private val searchQuery: String,
+    private val sources: String
+): PagingSource<Int, Article>() {
+
+    private var totalNewsCount = 0
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Article> {
+        val page = params.key ?: STARTING_PAGE_INDEX
+        return try {
+
+            val newsResponse = newsApi.searchNews(searchQuery = searchQuery, sources = sources, page = page)
+
+            totalNewsCount += newsResponse.articles.size
+            val articles = newsResponse.articles.distinctBy {it.title}
+
+            LoadResult.Page(
+                data = articles,
+                nextKey = if (totalNewsCount == newsResponse.totalResults) null else page + 1,
+                prevKey = null
+            )
+        }catch (e: Exception){
+            e.printStackTrace()
+            LoadResult.Error(e)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, Article>): Int? {
+        return  state.anchorPosition?.let { anchorPosition ->
+            val anchorPage = state.closestPageToPosition(anchorPosition)
+            anchorPage?.prevKey?.plus(1)?:anchorPage?.nextKey?.minus(1)
+        }
+    }
 }
